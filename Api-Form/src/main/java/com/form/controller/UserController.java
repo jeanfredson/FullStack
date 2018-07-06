aaf211d02e58ac094d7fa5b4668d82efb5f41d11
Rafael Carvalho <rapha.pse@outlook.com>
2018-07-06 20:55:16 -0300

    api restfull com spring data jap, spring boot

--- Api-Form/src/main/java/com/form/controller/UserController.java
+++ Api-Form/src/main/java/com/form/controller/UserController.java
@@ -11,6 +11,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -25,6 +26,7 @@ import com.form.repository.UserRepository;
 
 @RestController
 @RequestMapping("/api")
+@CrossOrigin(origins="http://localhost:4200", allowedHeaders="*")
 public class UserController {
 
 	
--- Api-Form/src/main/java/com/form/model/User.java
+++ Api-Form/src/main/java/com/form/model/User.java
@@ -1,151 +1,64 @@
 package com.form.model;
 
-import java.util.Date;
-
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
-import javax.persistence.Temporal;
-import javax.persistence.TemporalType;
-import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Email;
 import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
-
-import org.springframework.format.annotation.DateTimeFormat;
 
 @Entity
 @Table(name = "tabela_usuario")
 public class User {
 
 	@Id
-	@GeneratedValue(strategy=GenerationType.AUTO)
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Long id;
-	
-	@NotEmpty(message="Nome é obrigatório")
-	@Size(min=4, max=120)
+	@NotNull(message="Campo Obrigatório")
 	private String nome;
+	@Email(message="Email Invalido")
+	@NotNull(message="Email é obrigatório")
+	private String email;
+	@NotNull(message="Telefone é obrigatório")
+	private String telefone;
 	
-	@NotEmpty(message="Sobre Nome é obrigatório")
-	private String sobreNome;
-	
-	@NotEmpty(message="CPF é obrigatório")
-	private String cpf;
-	
-	@NotNull(message = "Data de Vencimento é obrigatório")
-	@DateTimeFormat(pattern = "dd/MM/yyy")
-	@Temporal(TemporalType.DATE)
-	private Date dataNascimento;
-
 	public User() {
-
+		// TODO Auto-generated constructor stub
 	}
-
-	public User(String nome, String sobreNome, String cpf, Date dataNascimento) {
-		
-		this.nome = nome;
-		this.sobreNome = sobreNome;
-		this.cpf = cpf;
-		this.dataNascimento = dataNascimento;
-	}
-
-	public User(Long id, String nome, String sobreNome, String cpf, Date dataNascimento) {
-		
+	public User(Long id, String nome, String email, String telefone) {
 		this.id = id;
 		this.nome = nome;
-		this.sobreNome = sobreNome;
-		this.cpf = cpf;
-		this.dataNascimento = dataNascimento;
+		this.email = email;
+		this.telefone = telefone;
 	}
+
+
 
 	public Long getId() {
 		return id;
 	}
-
 	public void setId(Long id) {
 		this.id = id;
 	}
-
 	public String getNome() {
 		return nome;
 	}
-
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
-
-	public String getSobreNome() {
-		return sobreNome;
+	public String getEmail() {
+		return email;
 	}
-
-	public void setSobreNome(String sobreNome) {
-		this.sobreNome = sobreNome;
+	public void setEmail(String email) {
+		this.email = email;
 	}
-
-	public String getCpf() {
-		return cpf;
+	public String getTelefone() {
+		return telefone;
 	}
-
-	public void setCpf(String cpf) {
-		this.cpf = cpf;
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
 	}
-
-	public Date getDataNascimento() {
-		return dataNascimento;
-	}
-
-	public void setDataNascimento(Date dataNascimento) {
-		this.dataNascimento = dataNascimento;
-	}
-
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((cpf == null) ? 0 : cpf.hashCode());
-		result = prime * result + ((dataNascimento == null) ? 0 : dataNascimento.hashCode());
-		result = prime * result + ((id == null) ? 0 : id.hashCode());
-		result = prime * result + ((nome == null) ? 0 : nome.hashCode());
-		result = prime * result + ((sobreNome == null) ? 0 : sobreNome.hashCode());
-		return result;
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		User other = (User) obj;
-		if (cpf == null) {
-			if (other.cpf != null)
-				return false;
-		} else if (!cpf.equals(other.cpf))
-			return false;
-		if (dataNascimento == null) {
-			if (other.dataNascimento != null)
-				return false;
-		} else if (!dataNascimento.equals(other.dataNascimento))
-			return false;
-		if (id == null) {
-			if (other.id != null)
-				return false;
-		} else if (!id.equals(other.id))
-			return false;
-		if (nome == null) {
-			if (other.nome != null)
-				return false;
-		} else if (!nome.equals(other.nome))
-			return false;
-		if (sobreNome == null) {
-			if (other.sobreNome != null)
-				return false;
-		} else if (!sobreNome.equals(other.sobreNome))
-			return false;
-		return true;
-	}
-
+	
+	
 }
