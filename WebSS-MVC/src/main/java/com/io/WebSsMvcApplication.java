c09e3177c4e2afdb790ef2b621f6211417874db4
Rafael <rapha.pse@outlook.com>
2018-09-09 22:17:57 -0300

    add

---
+++ WebSS-MVC/src/main/java/com/io/WebSsMvcApplication.java
@@ -0,0 +1,12 @@
+package com.io;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebSsMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebSsMvcApplication.class, args);
+	}
+}
