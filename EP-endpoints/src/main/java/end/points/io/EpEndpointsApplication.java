101e5e5cda0d504bb49abb34198059050907cb91
Rafael <rapha.pse@outlook.com>
2018-12-29 11:06:48 -0200

    add projeto

---
+++ EP-endpoints/src/main/java/end/points/io/EpEndpointsApplication.java
@@ -0,0 +1,12 @@
+package end.points.io;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EpEndpointsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EpEndpointsApplication.class, args);
+	}
+}
---
+++ EP-endpoints/src/test/java/end/points/io/EpEndpointsApplicationTests.java
@@ -0,0 +1,22 @@
+package end.points.io;
+
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EpEndpointsApplicationTests {
+
+
+
+	
+	@Test
+	public void create() {
+		
+				
+	}
+
+}
