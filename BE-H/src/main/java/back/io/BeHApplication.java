79fc4b5eae4e58b64f6a1dd02bf92f36aec1e6ab
Rafael <rapha.pse@outlook.com>
2018-12-29 11:06:28 -0200

    add projeto

---
+++ BE-H/src/main/java/back/io/BeHApplication.java
@@ -0,0 +1,12 @@
+package back.io;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeHApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeHApplication.class, args);
+	}
+}
